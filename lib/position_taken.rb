b3c31678fd748e22f85e934543de3e19a590f638
Phoebe Lau <phoebelau7@gmail.com>
2017-11-15 02:43:00 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -2,7 +2,7 @@
 def position_taken? (board, index)
   if board[index] != "" or board[index] != " " or board[index] != nil
     false
-  elsif board[index] == "X" && board[index] == "O"
+  elsif board[index] == "X"
     true
   end
 
